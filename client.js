ac5345cfad04771e134a34a66afde231f2ce4b22
yaswanthpaladugu <Yaswanth.Paladugu@berkadia.com>
2020-09-21 13:28:00 +0530

    initial commit

---
+++ client.js
@@ -0,0 +1,49 @@
+// This file contains the boilerplate to execute your React app.
+// If you want to modify your application's content, start in "index.js"
+
+import {ReactInstance, Surface} from 'react-360-web';
+
+function init(bundle, parent, options = {}) {
+  const r360 = new ReactInstance(bundle, parent, {
+    // Add custom options here
+    fullScreen: true,
+    ...options,
+  });
+
+  const buttonsPanel = new Surface(
+    400,
+    600,
+    Surface.SurfaceShape.Flat
+  );
+
+  buttonsPanel.setAngle(
+    -0.6,
+    0
+  );
+
+  r360.renderToSurface(
+    r360.createRoot('ConnectedButtonInfoPanel'),
+    buttonsPanel
+  );
+
+  const infoPanel = new Surface(
+    400,
+    400,
+    Surface.SurfaceShape.Flat
+  );
+
+  infoPanel.setAngle(
+    0.6,
+    0.1
+  );
+
+  r360.renderToSurface(
+    r360.createRoot('ConnectedHouseInfoPanel'),
+    infoPanel
+  );
+
+  // Load the initial environment
+  r360.compositor.setBackground(r360.getAssetURL('360_front_door.jpg'));
+}
+
+window.React360 = {init};
